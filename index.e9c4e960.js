0ebaa9b77cab88e2dd23c570448e91e880cbb293
Chalaya22 <130228713+Chalaya22@users.noreply.github.com>
2024-04-09 07:44:47 +0000

    Deploying to gh-pages from @ Chalaya22/form-fm@283005aa386ddf5878cb0e383264bc9ffaed8e7f 🚀

--- index.e9c4e960.js
+++
@@ -1,2 +0,0 @@
-var form=document.querySelector(".js-form");function onSubmit(e){e.preventDefault();e.currentTarget}console.dir(form),form.addEventListener("submit",onSubmit);
-//# sourceMappingURL=index.e9c4e960.js.map
