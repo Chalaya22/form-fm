283005aa386ddf5878cb0e383264bc9ffaed8e7f
Chalaya22 <chalayaolga22@gmail.com>
2024-04-09 10:44:21 +0300

    comit 5

--- src/script.js
+++ src/script.js
@@ -4,5 +4,45 @@ form.addEventListener('submit', onSubmit);
 
 function onSubmit(evt) {
   evt.preventDefault();
-  const form = evt.currentTarget;
+
+  const {
+    num,
+    typeList,
+    configList,
+    colorsList,
+    typeWashbasinList,
+    addOptions,
+  } = evt.currentTarget.elements;
+
+  const dataForm = {
+    length: num.value,
+    type: typeList.value,
+    config: colorsList.value,
+    size: typeWashbasinList.value,
+    addOptions: getCheckedCheckBoxes(),
+  };
+  console.log(dataForm);
 }
+
+function getCheckedCheckBoxes() {
+  const checkboxes = document.getElementsByClassName('checkbox');
+  console.log(checkboxes);
+  const checkboxesChecked = []; // можно в массиве их хранить, если нужно использовать
+  for (let index = 0; index < checkboxes.length; index++) {
+    if (checkboxes[index].checked) {
+      checkboxesChecked.push(checkboxes[index].value);
+      console.log(checkboxes[index].value); // положим в массив выбранный
+    }
+  }
+  return checkboxesChecked; // для использования как знначение в обьекте
+}
+
+// function getCheckedCheckBoxes() {
+//   var selectedCheckBoxes = document.querySelectorAll('input.checkbox:checked');
+
+//   var checkedValues = Array.from(selectedCheckBoxes).map(cb => cb.value);
+
+//   console.log(checkedValues);
+
+//   return checkedValues; // для использования в нужном месте
+// }
